e2f51cd789a67f69e87205ba0bd89aad3b003ce1
Sam <sample41010@gmail.com>
2024-10-08 12:01:02 -0400

    additional corrections

--- app.js
+++ app.js
@@ -46,6 +46,8 @@ app.get('/mode', function(req, res, next) {
         throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
     }
     let numsAsStrings = req.query.nums.split(',');
+    
+    let nums = convertAndValidateNumsArray(numsAsStrings);
     if (nums instanceof Error) {
         throw new ExpressError(nums.message);
     }
