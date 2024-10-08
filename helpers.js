8e75f8f6f8deda2300c2a5bc61370e368756928d
Sam <sample41010@gmail.com>
2024-10-08 11:48:19 -0400

    finished solution

---
+++ helpers.js
@@ -0,0 +1,80 @@
+ /** 
+ *@param {Array} arr 
+*/
+ function createFrequencyCounter(arr) {
+    return arr.reduce(function(acc, next) {
+        acc[next] = (acc[next] || 0) + 1;
+        return acc;
+    }, {});
+ }
+
+/**
+ * @param {Array} arr
+ */
+
+function findMode(){
+    let freqCounter= createFrequencyCounter(arr);
+
+    let count = 0;
+    let mostFrequent;
+
+    for (let key in freqCounter) {
+        if (freqCounter[key] > count) {
+            mostFrequent = key;
+            count = freqCounter[key];
+        }
+    }
+
+    return +mostFrequent;
+}
+
+/**
+ * @param {Array} numsAsStrings
+ * @returns {Arrray|Error}
+ */
+
+function convertAndValidateNumsArray(numsAsStrings) {
+    let result = [];
+
+    for (let i = 0; i < numsAsStrings.length; i++) {
+        let valtoNumber = (numsAsStrings[i]);
+
+        if (Number.isNaN(valToNumber)) {
+            return new Error(
+                `The value '${numsAsStrings[i]}' at index ${i} is not a valid number.`
+            );
+        }
+        result.push(valtoNumber);
+    }
+    return result;
+}
+
+function findMean(nums){
+    if(nums.length === 0) return 0;
+    return nums.reduce(function (acc, cur) {
+        return acc + cur;
+    }) / nums.length
+}
+
+function findMedian(nums){
+
+    nums.sort((a, b) => a - b);
+    
+    let middleIndex = Math.floor(nums.length / 2);
+    let median;
+
+    if (nums.length % 2 === 0) {
+        median = (nums[middleIndex] + nums[middleIndex - 1]) / 2;
+    } else {
+        median = nums[middleIndex];
+    }
+    return median
+}
+
+module.exports = {
+    createFrequencyCounter,
+    findMean,
+    findMedian,
+    findMode,
+    convertAndValidateNumsArray
+};
